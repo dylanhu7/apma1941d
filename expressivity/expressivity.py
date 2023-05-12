98d4cf44338ebf172171cc75def0267748f7cddc
Dylan Hu <git@dylan.hu>
2023-05-12 00:00:32 -0400

    upload

--- expressivity/expressivity.py
+++ expressivity/expressivity.py
@@ -1,6 +1,5 @@
 import argparse
 import csv
-import glob
 import os
 
 import matplotlib.pyplot as plt
@@ -57,20 +56,10 @@ class PositionalEncoding(nn.Module):
             encoding_fns.append(
                 lambda x, i=i: torch.cos(2 ** i * x))
         return torch.cat([fn(x) for fn in encoding_fns], dim=-1)
-    
-    def fourier_encoding(self, x):
-        """
-        Encode the input tensor using random Fourier features
-        """
-        for i in range(self.num_bands):
-            x = torch.cat((torch.sin((2**i)*x), torch.cos((2**i)*x)), dim=-1)
-        return x
 
     def forward(self, x):
         if self.mode == 'sincos':
             return self.sincos_encoding(x)
-        elif self.mode == 'fourier':
-            return self.fourier_encoding(x)
         else:
             raise ValueError(f'Unknown mode: {self.mode}')
     
@@ -110,29 +99,49 @@ def load_data(file_path) -> list[tuple[Tensor, Tensor]]:
             data.append((x, y))
     return data
 
-def plot_losses(losses: list[float]):
+def plot_losses(losses: list[float], args: Args, i: int):
     plt.plot(losses)
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title('Training Loss')
-    plt.show()
+    # add depth text to plot
+    plt.text(0.1, 0.9, f'Depth: {args.depth}', transform=plt.gca().transAxes, color='gray')
+    plt.text(0.1, 0.85, f'Width: {args.width}', transform=plt.gca().transAxes, color='gray')
+    plt.text(0.1, 0.8, f'Activation function: {args.activation_fn}', transform=plt.gca().transAxes, color='gray')
+    plt.text(0.1, 0.75, f'Learning rate: {args.lr}', transform=plt.gca().transAxes, color='gray')
+    dirname = get_dirname(args)
+    filename = os.path.join(dirname, f'{i}.png')
+    plt.savefig(filename)
+    plt.close()
 
-def animate_training(x: Tensor, ground_truth: Tensor, predictions: list[Tensor], losses: list[float], args: Args):
+def animate_training(x: Tensor, ground_truth: Tensor, predictions: list[Tensor], losses: list[float], args: Args, i: int):
     fig, ax = plt.subplots()
     line1, = ax.plot(x, ground_truth.detach().numpy(), label='Ground truth')  # Plot the true values
     line2, = ax.plot(x, predictions[0], label='Model approximation')  # Plot the model's initial predictions
     plt.legend()
 
-    epoch_text = ax.text(0.02, 0.8, '', transform=ax.transAxes, color='black')
-    depth_text = ax.text(0.02, 0.75, f'Depth: {args.depth}', transform=ax.transAxes, color='gray')
-    width_text = ax.text(0.02, 0.7, f'Width: {args.width}', transform=ax.transAxes, color='gray')
-    activation_fn_text = ax.text(0.02, 0.65, f'Width: {args.width}', transform=ax.transAxes, color='gray')
-    lr_text = ax.text(0.02, 0.6, f'Learning rate: {args.activation_fn}', transform=ax.transAxes, color='gray')
-    num_bands_text = ax.text(0.02, 0.55, f'Number of bands: {args.pe_num_bands}', transform=ax.transAxes, color='gray') if args.use_positional_encoding else None
+    position_dict = {
+        0: (0.02, 0.8),
+        1: (0.6, 0.8),
+        2: (0.02, 0.8),
+        3: (0.59, 0.42),
+        4: (0.02, 0.8),
+    }
+
+    x_pos, y_pos = position_dict[i]
+
+    epoch_text = ax.text(x_pos, y_pos, '', transform=ax.transAxes, color='black')
+    loss_text = ax.text(x_pos, y_pos - 0.05, '', transform=ax.transAxes, color='black')
+    depth_text = ax.text(x_pos, y_pos - 0.1, f'Depth: {args.depth}', transform=ax.transAxes, color='gray')
+    width_text = ax.text(x_pos, y_pos - 0.15, f'Width: {args.width}', transform=ax.transAxes, color='gray')
+    activation_fn_text = ax.text(x_pos, y_pos - 0.2, f'Width: {args.width}', transform=ax.transAxes, color='gray')
+    lr_text = ax.text(x_pos, y_pos - 0.25, f'Learning rate: {args.activation_fn}', transform=ax.transAxes, color='gray')
+    num_bands_text = ax.text(x_pos, y_pos - 0.3, f'Number of bands: {args.pe_num_bands}', transform=ax.transAxes, color='gray') if args.use_positional_encoding else None
 
     def update(num):
         line2.set_ydata(predictions[num])  # Update the model's predictions
         epoch_text.set_text(f'Epoch: {num + 1}')  # Update the epoch counter
+        loss_text.set_text(f'Loss: {losses[num]}')  # Update the loss counter
         depth_text.set_text(f'Depth: {args.depth}')
         width_text.set_text(f'Width: {args.width}')
         activation_fn_text.set_text(f'Activation function: {args.activation_fn}')
@@ -145,20 +154,20 @@ def animate_training(x: Tensor, ground_truth: Tensor, predictions: list[Tensor],
     plt.close(fig)
     return animation
 
-def get_filename(args: Args, i: int):
+def get_dirname(args: Args):
     name_components: list[str] = []
     name_components.append("d" + str(args.depth))
     name_components.append("w" + str(args.width))
     if args.use_positional_encoding:
         name_components.append(args.pe_mode)
         name_components.append("b" + str(args.pe_num_bands))
-    name_components.append(str(i))
-    return "_".join(name_components)
+    return args.output_dir + "/" + "_".join(name_components)
 
 def write_animation(animation: FuncAnimation, args: Args, i: int):
-    filename = get_filename(args, i)
-    outfile = os.path.join(args.output_dir, f"{filename}.mp4")
-    animation.save(outfile, writer='ffmpeg', fps=50)
+    dirname = get_dirname(args)
+    os.makedirs(dirname, exist_ok=True)
+    filename = os.path.join(dirname, f'{i}.mp4')
+    animation.save(filename, writer='ffmpeg', fps=50)
 
 def activation_fn(name: str):
     if name == 'none':
@@ -175,6 +184,7 @@ def activation_fn(name: str):
         return nn.Tanh()
     else:
         raise ValueError(f'Unknown activation function: {name}')
+    
 
 
 def main(args: Args):
@@ -187,12 +197,13 @@ def main(args: Args):
         if args.use_positional_encoding:
             x_enc = encoder(x)
             input_dim = x_enc.shape[-1]
-            model = MultiLayerPerceptron([input_dim, 16, 16, 16, 1], activation_fn=activation_fn(args.activation_fn))
+            model = MultiLayerPerceptron([input_dim] + [args.width] * args.depth + [1], activation_fn=activation_fn(args.activation_fn))
             predictions, losses = train(model, x_enc, y, epochs=1000, learning_rate=args.lr)
         else:
-            model = MultiLayerPerceptron([1, 16, 16, 16, 1], activation_fn=nn.ELU())
+            model = MultiLayerPerceptron([1] + [args.width] * args.depth + [1], activation_fn=nn.ELU())
             predictions, losses = train(model, x, y, epochs=1000, learning_rate=0.001)
-        write_animation(animate_training(x, y, predictions, losses, args), args, i)
+        write_animation(animate_training(x, y, predictions, losses, args, i), args, i)
+        plot_losses(losses, args, i)
         
 
 if __name__ == '__main__':
